dc31683fe740e04e72ba93b9111d53d8603239d6
Danylo <nunesdanylo16@gmail.com>
2023-01-27 21:21:37 -0300

    list products

--- js/api.js
+++ js/api.js
@@ -1,24 +1,41 @@
-
 async function pegarProdutos() {
   const response = await fetch("http://localhost:3000/produtos");
 
   const data = await response.json();
 
-  const postContainer = document.querySelector(".container-teste");
+  const stars = (numberStars) => {
+    let star = '';
 
-
-  data.map((post) => {
-    const div = document.createElement("div");
-    const nome = document.createElement("span");
-
-    nome.innerText = post.productName;
-
-    div.appendChild(nome)
-
-    postContainer.appendChild(div);
+      for (let i = 0; i < 5; i++){
+        if (i < numberStars) {
+          star += '<i class="fa-solid fa-star" style="color: rgb(237, 85, 85);"></i>'
+        }
+        else {
+          star+= '<i class="fa-regular fa-star" id="primeira-estrela" style="color: rgb(237, 85, 85);"></i>'
+        }
+      }
+      return star
     
-  })
+    
+  }
+
+  const convertMoney = (price) => {
+    const formater = new Intl.NumberFormat('pt-BR', {style:'currency',currency:'BRL',});
+    return formater.format(price);
+  }
+
+  const produtos = data.map((produto) => {
+    return `<div class="box">
+    <img src="${produto.imageUrl}" alt="" id="img-produto">
+    <span id="nome-produto">${produto.productName}</span>
+    <div class="estrelas" id="avaliacao-produto">${stars(produto.stars)} </div>
+    <span id="preco-produto">por ${convertMoney(produto.price)}</span>
+    <span id="parcela-produto">ou em ${produto.installments[0].quantity} de ${produto.installments[0].value}</span>
+    <button class="botao-comprar">COMPRAR</button>
+  </div>`;
+  });
+  console.log(produtos);
+  document.querySelector(".container-produtos").innerHTML = produtos
 }
 
 pegarProdutos();
-
